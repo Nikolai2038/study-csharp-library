29129cdeb88d301a53223364e4531cb9ba828f6a
Nikolai-415 <nikolai-415@yandex.ru>
2022-05-03 00:46:37 +0300

    Добавил обработку исключений при загрузке формы

--- CSharpStudySolution/CSharpStudyNetFramework/Forms/Form_Data.cs
+++ CSharpStudySolution/CSharpStudyNetFramework/Forms/Form_Data.cs
@@ -1,18 +1,36 @@
 ﻿using CSharpStudyNetFramework.Helpers;
+using MetroFramework;
 using MetroFramework.Forms;
+using System;
+using System.Windows.Forms;
 
 namespace CSharpStudyNetFramework.Forms
 {
+    /// <summary>Форма с данными</summary>
     public partial class Form_Data : MetroForm
     {
+        /// <summary>Конструктор формы</summary>
         public Form_Data()
         {
             this.InitializeComponent();
         }
 
-        private void Form_Data_Load(object sender, System.EventArgs e)
+        /// <summary>Событие загрузки формы</summary>
+        private void Form_Data_Load(object sender, EventArgs e)
         {
-            this.Grid_Data.DataSource = DatabaseHelper.db_context.authors;
+            // В случае возникновения ошибки, повторяем скрипт пока выбирается "Retry"
+            do {
+                try {
+                    this.Grid_Data.DataSource = DatabaseHelper.db_context.authors;
+                } catch (Exception exception) {
+                    DialogResult dialogResult = MetroMessageBox.Show(this, exception.Message, "Возникла ошибка!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    // Если была выбрана "Cancel"
+                    if (dialogResult == DialogResult.Cancel) {
+                        // Выход из программы
+                        Environment.Exit(1);
+                    }
+                }
+            } while (true);
         }
     }
 }
