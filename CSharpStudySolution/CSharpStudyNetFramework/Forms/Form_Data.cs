be118c8cc9abaeb89056aba28087f255c854e4ff
Nikolai-415 <nikolai-415@yandex.ru>
2022-05-03 01:14:53 +0300

    Сделал рабочее подключение данных

--- CSharpStudySolution/CSharpStudyNetFramework/Forms/Form_Data.cs
+++ CSharpStudySolution/CSharpStudyNetFramework/Forms/Form_Data.cs
@@ -1,7 +1,9 @@
 ﻿using CSharpStudyNetFramework.Helpers;
+using CSharpStudyNetFramework.ORM;
 using MetroFramework;
 using MetroFramework.Forms;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace CSharpStudyNetFramework.Forms
@@ -21,13 +23,19 @@ namespace CSharpStudyNetFramework.Forms
             // В случае возникновения ошибки, повторяем скрипт пока выбирается "Retry"
             do {
                 try {
-                    this.Grid_Data.DataSource = DatabaseHelper.db_context.authors;
+                    CustomDbContext db_context = new CustomDbContext();
+                    this.Grid_Data.DataSource = db_context.authors.ToList();
+                    break;
                 } catch (Exception exception) {
-                    DialogResult dialogResult = MetroMessageBox.Show(this, exception.Message, "Возникла ошибка!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    DialogResult dialogResult = MetroMessageBox.Show(this, exception.Message, "Возникла ошибка!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
                     // Если была выбрана "Cancel"
-                    if (dialogResult == DialogResult.Cancel) {
+                    if (dialogResult == DialogResult.Abort) {
                         // Выход из программы
                         Environment.Exit(1);
+                    }
+                    else if (dialogResult == DialogResult.Ignore) {
+                        // Продолжение выполнения программы
+                        break;
                     }
                 }
             } while (true);
--- CSharpStudySolution/CSharpStudyNetFramework/ORM/Models/Group.cs
+++ CSharpStudySolution/CSharpStudyNetFramework/ORM/Models/Group.cs
@@ -2,5 +2,7 @@
 {
     internal class Group
     {
+        public int? Id { get; set; }
+        public string Title { get; set; }
     }
 }
